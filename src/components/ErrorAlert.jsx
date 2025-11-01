9a61b4bc713a35cc7b1505c0b9e27b9834c5e28f
Akash Maity <akashmaity4452@gmail.com>
2025-11-01 10:12:19 +0530

    errorAlert

---
+++ src/components/ErrorAlert.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ErrorAlert = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          TAG Movie Portal
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarColor01"
+          aria-controls="navbarColor01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default ErrorAlert;
